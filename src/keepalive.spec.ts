706dfecbe31af5eac3a145ecadbea6f80965eb80
Mike Grabski <me@mikegrabski.com>
2016-02-08 01:08:06 -0500

    feat(Keepalive): add ping functionality * Automatically ping HTTP endpoint when a request is configured * Pinging can be done on demand or on a configurable interval with start/stop * Emits onPing event when pinging is occuring (regardless of HTTP config) * Emits onPingReponse event when HTTP is configured and there is a response

--- src/keepalive.spec.ts
+++ src/keepalive.spec.ts
@@ -1,13 +1,200 @@
-import {it, inject, beforeEachProviders} from 'angular2/testing';
+import {
+  afterEach,
+  it,
+  inject,
+  injectAsync,
+  fakeAsync,
+  beforeEach,
+  beforeEachProviders,
+  tick
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {
+  BaseRequestOptions,
+  Http,
+  HTTP_PROVIDERS,
+  Request,
+  RequestMethod,
+  ResponseOptions,
+  Response
+} from 'angular2/http';
+import {MockBackend} from 'angular2/http/testing';
 
 import {Keepalive} from './keepalive';
 
 export function main() {
   describe('Keepalive', () => {
-    beforeEachProviders(() => [Keepalive]);
 
-    it('sayHello() should say hello to specified name', inject([Keepalive], (keepalive) => {
-         expect(keepalive.sayHello('Mike')).toBe('Hello, Mike!');
-       }));
+    beforeEachProviders(
+        () => [BaseRequestOptions, HTTP_PROVIDERS, MockBackend, Keepalive, provide(Http, {
+                 deps: [MockBackend, BaseRequestOptions],
+                 useFactory:
+                     (backend, defaultOptions) => { return new Http(backend, defaultOptions); }
+               })]);
+
+    let instance: Keepalive;
+
+    beforeEach(inject([Keepalive], keepalive => { instance = keepalive; }));
+
+    describe('runtime config', () => {
+      it('request() should set and return a Request', () => {
+        let expected = new Request({method: RequestMethod.Get, url: 'http://test.com'});
+
+        let actual = instance.request(expected);
+
+        expect(actual).toEqual(expected);
+      });
+
+      it('request() should set and return a Request from a string URL', () => {
+        let expected = 'http://test.com/2';
+        let actual = instance.request(expected);
+
+        expect(actual.method).toEqual(RequestMethod.Get);
+        expect(actual.url).toEqual(actual.url);
+      });
+
+      it('request() with null URL should set request to null', () => {
+        let actual = instance.request(null);
+
+        expect(actual).toBeNull();
+        expect(instance.request()).toBeNull();
+      });
+
+      it('request() with no params should return current value', () => {
+        let expected: string = instance.request('https://test.com/3').url;
+
+        expect(instance.request().url).toEqual(expected);
+      });
+
+      it('interval() should set and return a numeric value (representing seconds) greater than 0',
+         () => {
+           let expected = 10;
+           let actual = instance.interval(expected);
+
+           expect(actual).toEqual(expected);
+         });
+
+      it('interval() with no params should return current value', () => {
+        let expected = 10;
+        instance.interval(expected);
+
+        let actual = instance.interval();
+
+        expect(actual).toEqual(expected);
+      });
+
+      it('interval() should throw if the value is less than or equal to zero', () => {
+        expect(() => {
+          instance.interval(0);
+        }).toThrow(new Error('Interval value must be greater than zero.'));
+
+        expect(() => {
+          instance.interval(-1);
+        }).toThrow(new Error('Interval value must be greater than zero.'));
+      });
+    });
+
+    describe('without using HTTP request', () => {
+      beforeEach(() => { instance.request(null); });
+
+      it('ping() should emit onPing event', injectAsync([], () => {
+           return new Promise((pass, fail) => {
+             instance.onPing.subscribe(() => { pass(); });
+
+             instance.ping();
+           });
+         }), 300);
+    });
+
+    describe('using an HTTP request', () => {
+
+      let request = new Request({method: RequestMethod.Get, url: 'https://test.com'});
+      let backend: MockBackend;
+
+      beforeEach(inject([Keepalive, MockBackend], (keepalive, mockBackend) => {
+        backend = mockBackend;
+        instance = keepalive;
+        instance.request(request);
+      }));
+
+      afterEach(() => {
+        // TODO: should be throwing if no expected requests are made
+        backend.resolveAllConnections();
+        backend.verifyNoPendingRequests();
+      });
+
+      it('ping() should fire request and emit onPingResponse event', injectAsync([], () => {
+           return new Promise((pass, fail) => {
+             backend.connections.subscribe(connection => {
+               expect(connection.request.url).toBe(request.url);
+
+               connection.mockRespond(new ResponseOptions({status: 200}));
+             });
+
+             instance.onPingResponse.subscribe((response: Response) => {
+               expect(response.status).toBe(200);
+               pass();
+             });
+
+             instance.ping();
+           });
+         }), 300);
+    });
+
+    describe('on an interval', () => {
+      beforeEach(() => {
+        instance.interval(5);
+        spyOn(instance, 'ping').and.callThrough();
+      });
+
+      it('start() should schedule and ping at the specified interval', <any>fakeAsync((): void => {
+           instance.start();
+
+           tick(1000);
+           expect(instance.ping).not.toHaveBeenCalled();
+
+           tick(4000);
+           expect(instance.ping).toHaveBeenCalledTimes(1);
+
+           tick(5000);
+           expect(instance.ping).toHaveBeenCalledTimes(2);
+
+           // must call stop to clear intervals, otherwise fake_async will
+           // throw "1 periodic timer(s) still in the queue."
+           instance.stop();
+         }));
+
+      it('stop() should cease pinging', <any>fakeAsync((): void => {
+           instance.start();
+           instance.stop();
+
+           tick(5000);
+           expect(instance.ping).not.toHaveBeenCalled();
+         }));
+
+      it('start() after calling start() cancels the previous interval', <any>fakeAsync((): void => {
+           instance.start();
+           instance.interval(10);
+           instance.start();
+
+           tick(5000);
+           expect(instance.ping).not.toHaveBeenCalled();
+
+           tick(5000);
+           expect(instance.ping).toHaveBeenCalledTimes(1);
+
+           instance.stop();
+         }));
+
+      it('ngOnDestroy() invokes stop()', <any>fakeAsync((): void => {
+           spyOn(instance, 'stop').and.callThrough();
+
+           instance.start();
+
+           instance.ngOnDestroy();
+
+           expect(instance.stop).toHaveBeenCalled();
+         }));
+    });
   });
 }
--- src/keepalive.ts
+++ src/keepalive.ts
@@ -1,14 +1,98 @@
-import {Injectable} from 'angular2/core';
+import {Injectable, EventEmitter, OnDestroy} from 'angular2/core';
+import {Http, Request, RequestMethod, Response} from 'angular2/http';
 
 /**
  * An example of an injectable service.
  */
 @Injectable()
-export class Keepalive {
-  /**
-   * Returns a greeting using the specified name.
-   * @param name - The name to use in the greeting.
-   * @return The greeting.
+export class Keepalive implements OnDestroy {
+  private pingRequest: Request;
+  private pingInterval: number = 10 * 60;
+  private pingHandle: any;
+
+  /*
+   * An event emitted when the service is pinging.
    */
-  sayHello(name: string): string { return 'Hello, ' + name + '!'; }
+  public onPing: EventEmitter<any> = new EventEmitter;
+
+  /*
+   * An event emitted when the service has pinged an HTTP endpoint and received a response.
+   */
+  public onPingResponse: EventEmitter<Response> = new EventEmitter;
+
+  /*
+   * Initializes a new instance of Keepalive
+   * @param http - The HTTP service.
+   */
+  constructor(private http: Http) {}
+
+  /*
+   * Sets the string or Request that should be used when pinging.
+   * @param url - The URL or Request object to use when pinging.
+   * @return The current Request used when pinging.
+   */
+  request(url?: string | Request): Request {
+    if (typeof url === 'string') {
+      this.pingRequest = new Request({method: RequestMethod.Get, url: url});
+    } else if (url instanceof Request) {
+      this.pingRequest = url;
+    } else if (url === null) {
+      this.pingRequest = null;
+    }
+
+    return this.pingRequest;
+  }
+
+  /*
+   * Sets the interval (in seconds) at which the ping operation will occur when start() is called.
+   * @param seconds - The ping interval in seconds.
+   * @return The current interval value.
+   */
+  interval(seconds?: number): number {
+    if (!isNaN(seconds) && seconds > 0) {
+      this.pingInterval = seconds;
+    } else if (!isNaN(seconds) && seconds <= 0) {
+      throw new Error('Interval value must be greater than zero.');
+    }
+
+    return this.pingInterval;
+  }
+
+  /*
+   * Immediately performs the ping operation. If a request has been set, an HTTP
+   * request will be made and the response will be emitted via the
+   * onPingResponse event.
+   */
+  ping(): void {
+    this.onPing.emit(null);
+    if (this.pingRequest) {
+      this.http.request(this.pingRequest).subscribe((response: Response) => {
+        this.onPingResponse.emit(response);
+      });
+    }
+  }
+
+  /*
+   * Starts pinging on an interval.
+   */
+  start(): void {
+    this.stop();
+
+    this.pingHandle = setInterval(() => { this.ping(); }, this.pingInterval * 1000);
+  }
+
+  /*
+   * Stops pinging on an interval.
+   */
+  stop(): void {
+    if (this.pingHandle) {
+      clearInterval(this.pingHandle);
+      this.pingHandle = null;
+    }
+  }
+
+  /*
+   * Performs any cleanup tasks when Angular destroys the instance.
+   */
+  ngOnDestroy(): void { this.stop(); }
 }
