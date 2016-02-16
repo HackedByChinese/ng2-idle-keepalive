a7e75dae7d12afe6edaf2836170bf778b8e6778e
Mike Grabski <me@mikegrabski.com>
2016-02-16 02:22:45 -0500

    fix(Core): use existing instance of Keepalive when registering KeepaliveSvc
    Previously KEEPALIVE_PROVIDERS was registering Keepalive and KeepaliveSvc as two separate instances

--- src/core.spec.ts
+++ src/core.spec.ts
@@ -5,8 +5,8 @@ import {
 } from 'angular2/testing';
 import {HTTP_PROVIDERS} from 'angular2/http';
 
-import {Idle, IDLE_PROVIDERS} from 'ng2-idle/core';
-import {KEEPALIVE_PROVIDERS} from './core';
+import {Idle, IDLE_PROVIDERS, KeepaliveSvc} from 'ng2-idle/core';
+import {KEEPALIVE_PROVIDERS, Keepalive} from './core';
 
 export function main() {
   describe('core', () => {
@@ -14,5 +14,10 @@ export function main() {
 
     it('KEEPALIVE_PROVIDERS should register Idle-compatible KeepaliveSvc provider',
        inject([Idle], (idle: Idle) => { expect(idle.getKeepaliveEnabled()).toBe(true); }));
+
+    it('KEEPALIVE_PROVIDERS should register the same instance for Keepalive and KeepaliveSvc',
+       inject([Keepalive, KeepaliveSvc], (k1: Keepalive, k2: KeepaliveSvc) => {
+         expect(k1).toBe(k2);
+       }));
   });
 }
--- src/core.ts
+++ src/core.ts
@@ -4,6 +4,7 @@ import {provide} from 'angular2/core';
 
 export * from './keepalive';
 
-export const KEEPALIVE_PROVIDERS: any[] = [Keepalive, provide(KeepaliveSvc, {useClass: Keepalive})];
+export const KEEPALIVE_PROVIDERS: any[] =
+    [Keepalive, provide(KeepaliveSvc, {useExisting: Keepalive})];
 
 export default {providers: [KEEPALIVE_PROVIDERS]}
